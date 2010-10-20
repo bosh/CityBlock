37b3a6139330a2e783831a2720710887c9c5594a
Matthew Rathbone <matthew@dropio.com>
2010-10-20 18:13:47 -0400

    it compiles and runs, doesnt do anything yet though

---
+++ lib/CityBlockGame.java
@@ -0,0 +1,16 @@
+import game.*;
+import java.awt.*;
+public class CityBlockGame extends Platform{
+	
+	public void setup(){
+		
+	}
+	public void update(){
+		
+	}
+
+   public void overlay(Graphics g) {   
+   }
+
+	
+}
