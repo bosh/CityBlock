ddb898de7b230b1198774dabd811291416c2dce9
Bosh <ohwaitnvm a gmail d com>
2010-10-26 19:58:29 -0400

    fixing LineSegment distance calculation, added x and y min and maxes

--- lib/game/LineSegment.java
+++ lib/game/LineSegment.java
@@ -13,22 +13,21 @@ public class LineSegment {
 	public double[] distanceTo(LineSegment other){ //Only returns parallel line distances
 		double[] distance = new double[] {999,999};
 		if (isHorizontal() && other.isHorizontal()) {
-			if (true) {
+			if ((x1 >= other.xMin() && x1 <= other.xMax()) || (x2 >= other.xMin() && x2 <= other.xMax()) || (other.x1 >= xMin() && other.x1 <= xMax()) || (other.x2 >= xMin() && other.x2 <= xMax())) {
 				distance[1] = y1 - other.y1;
 			}
 		} else if (isVertical() && other.isVertical()){
-			if (true) {
+			if ((y1 >= other.yMin() && y1 <= other.yMax()) || (y2 >= other.yMin() && y2 <= other.yMax()) || (other.y1 >= yMin() && other.y1 <= yMax()) || (other.y2 >= yMin() && other.y2 <= yMax())) {
 				distance[0] = x1 - other.x1;
 			}
 		}
 		return distance;
 	}
 
-	public boolean isHorizontal() {
-		return (y1 == y2);
-	}
-
-	public boolean isVertical() {
-		return (x1 == x2);
-	}
+	public boolean isHorizontal() { return (y1 == y2); }
+	public boolean isVertical() { return (x1 == x2); }
+	public double xMin() { return Math.min(x1, x2); }
+	public double xMax() { return Math.max(x1, x2); }
+	public double yMin() { return Math.min(y1, y2); }
+	public double yMax() { return Math.max(y1, y2); }
 }
