e6f90691656b015b14a9886bc94bb20d6a7c7b2d
bosh <ohwaitnvm@gmail.com>
2010-12-03 23:45:12 -0500

    added rectangle and made rectshape display it

--- lib/cityblock/RectShape.java
+++ lib/cityblock/RectShape.java
@@ -2,13 +2,22 @@ package cityblock;
 import java.awt.*;
 import game.*;
 import java.util.*;
-public class RectShape extends cityblock.Shape {
+import java.awt.image.*;
+
+public class RectShape extends cityblock.Shape implements ImageObserver{
+	public Image img;
 	
 	public RectShape(int w, int h){
 		super(w, h);
 		defaultColor = Color.blue;
+		img = Platform.platform.getImage(Platform.platform.getCodeBase(), "rectangle.png");
 	}
-	
+
+	public void update(Graphics g) {
+		super.update(g);
+		g.drawImage(img, (int) getX() - width/2, (int) getY() - height/2, width, height, this);
+	}
+
 	public static Shape randomize(){
 		Random r = new Random();
 		int dimension = cityblock.Shape.dimMultiplier * (r.nextInt(cityblock.Shape.maxDimension) + 1); //between 1 and maxDimension
@@ -17,20 +26,22 @@ public class RectShape extends cityblock.Shape {
 		RectShape result = new RectShape(dimension, dimension2);
 		return result;
 	}
-	
-	
+
 	public void setup(int x, int y){
-	  this.x = x + width / 2;
-      this.y = y + height / 2;
-      X[0] = x;
-      Y[0] = y;
-      X[1] = x + width;
-      Y[1] = y;
-      X[2] = x + width;
-      Y[2] = y + height;
-      X[3] = x;
-      Y[3] = y + height;
-      setShape(X, Y, 4);	
+		this.x = x + width / 2;
+		this.y = y + height / 2;
+		X[0] = x;
+		Y[0] = y;
+		X[1] = x + width;
+		Y[1] = y;
+		X[2] = x + width;
+		Y[2] = y + height;
+		X[3] = x;
+		Y[3] = y + height;
+		setShape(X, Y, 4);
 	}
-	
+
+	public boolean imageUpdate(Image img, int x, int y, int width, int height, int fudge){
+		return true;
+	}
 }
